3aeb7f132432361193d0a71e322084b29d769a39
maria <mariacabrera03@gmail.com>
2025-08-31 16:24:43 -0300

    Se agregan metodos a las interfaces DAO

--- Integrador 1/EjercicioIntegrador1/src/main/java/org/example/dao/ClienteDAO.java
+++ Integrador 1/EjercicioIntegrador1/src/main/java/org/example/dao/ClienteDAO.java
@@ -1,4 +1,10 @@
 package org.example.dao;
 
+import org.example.entity.Cliente;
+
+import java.util.List;
+
 public interface ClienteDAO {
+    List<Cliente> findAll();
+    void add(List<Cliente> c);
 }
--- Integrador 1/EjercicioIntegrador1/src/main/java/org/example/dao/FacturaDAO.java
+++ Integrador 1/EjercicioIntegrador1/src/main/java/org/example/dao/FacturaDAO.java
@@ -1,4 +1,9 @@
 package org.example.dao;
 
+import org.example.entity.Factura;
+
+import java.util.List;
+
 public interface FacturaDAO {
+    void add(List<Factura> f);
 }
