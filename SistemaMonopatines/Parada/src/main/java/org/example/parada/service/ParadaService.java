404c08729a2b6db4307aef633828664759808a71
EliGramuglia <eliana.gramuglia@gmail.com>
2025-11-02 20:37:42 -0300

    Se agrega estructura para el microservicio Parada

---
+++ SistemaMonopatines/Parada/src/main/java/org/example/parada/service/ParadaService.java
@@ -0,0 +1,9 @@
+package org.example.parada.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class ParadaService {
+}
