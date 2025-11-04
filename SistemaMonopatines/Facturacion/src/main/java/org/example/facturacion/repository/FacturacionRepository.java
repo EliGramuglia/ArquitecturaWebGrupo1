1659704b8c3cfb607180ebc9bb2dce46b701261b
EliGramuglia <eliana.gramuglia@gmail.com>
2025-11-03 21:32:34 -0300

    Se elimina MSV Facturación, ya que es redundante

--- SistemaMonopatines/Facturacion/src/main/java/org/example/facturacion/repository/FacturacionRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.facturacion.repository;
-
-import org.example.facturacion.entity.Facturacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacturacionRepository extends JpaRepository<Facturacion, Long> {
-
-}
