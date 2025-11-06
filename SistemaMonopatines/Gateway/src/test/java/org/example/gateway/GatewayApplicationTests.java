af53591ddbd0a273147c1d5724e4431cc9570a94
Maria Cabrera <mariacabrera03@gmail.com>
2025-11-06 20:44:23 -0300

    Se agrega docker monopatin, se borran carpetas extras

--- SistemaMonopatines/Gateway/src/test/java/org/example/gateway/GatewayApplicationTests.java
+++ SistemaMonopatines/Gateway/src/test/java/org/example/gateway/GatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.tarifa;
+package org.example.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TarifaApplicationTests {
+class GatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- SistemaMonopatines/Tarifa/src/main/java/org/example/tarifa/repository/TarifaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.tarifa.repository;
-
-import org.example.tarifa.entity.Tarifa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TarifaRepository extends JpaRepository<Tarifa, Long> {
-}
