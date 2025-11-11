f2f38587788f6a5c4671a274347ee5c623051d98
EliGramuglia <eliana.gramuglia@gmail.com>
2025-11-11 17:20:03 -0300

    Se agrega driagrama de subdominios (muestra los microservicios y las entidades principales

--- SistemaMonopatines/Viaje/src/main/java/org/example/viaje/entity/Tarifa.java
+++ SistemaMonopatines/Viaje/src/main/java/org/example/viaje/entity/Tarifa.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Entity
-public class Tarifa {
+public class    Tarifa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
